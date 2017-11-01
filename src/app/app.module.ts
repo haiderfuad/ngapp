f5d7c2d542d5ecb069726f4accbe5d25858bc003
haiderfuad <haiderfuad@yahoo.com>
2017-11-01 13:39:25 +0000

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule, MatCardModule, MatToolbarModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ContentService } from './shared/services/content.service';
+import { AppRoutingModule } from './app-routing.module';
 
 
 import { AppComponent } from './app.component';
@@ -19,7 +20,8 @@ import { PageComponent } from './page/page.component';
     BrowserModule,
     MatButtonModule,
     MatCardModule,
-    MatToolbarModule
+    MatToolbarModule,
+    AppRoutingModule
   ],
   providers: [ContentService],
   bootstrap: [AppComponent]
--- src/app/shared/services/content.service.ts
+++ src/app/shared/services/content.service.ts
@@ -5,7 +5,7 @@ export class ContentService {
   pages: Object = {
     'home': {title: 'Home', subtitle: 'Welcome Home!', content: 'Some home content.', image: 'assets/mamp.png'},
     'about': {title: 'About', subtitle: 'About Us', content: 'Some content about us', image: 'assets/gimp.png'},
-    'contact': {title: 'Contact', subtitle: 'Contact Us', content: 'How to contact us.', image: 'assets/submarine_blur.png'}
+    'contact': {title: 'Contact', subtitle: 'Contact Us', content: 'How to contact us.', image: 'assets/Submarine_blur.png'}
   };
   constructor() { }
 
